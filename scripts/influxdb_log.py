c77817bc181b5dbc791e3eb72b8a672a099355a0
Don Neill <neilljd@gmail.com>
2025-10-20 09:25:59 -0700

    Fix infinite handlers

--- scripts/influxdb_log.py
+++ scripts/influxdb_log.py
@@ -29,7 +29,8 @@ def main(config_file):
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler = logging.FileHandler(cfg['logfile'])
         file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        if not logger.hasHandlers():
+            logger.addHandler(file_handler)
     else:
         logger = None
 
